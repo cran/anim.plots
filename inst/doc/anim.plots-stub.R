fbf16c81759551a5f73072808e51ab9a9f0ffff9
David Hugh-Jones <davidhughjones@gmail.com>
2019-05-16 14:00:03 +0000

    version 0.2.1

--- inst/doc/anim.plots-stub.R
+++ inst/doc/anim.plots-stub.R
@@ -2,7 +2,7 @@
 knitr::opts_chunk$set(echo = TRUE)
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  rmd_file <- system.file('doc', 'anim.plots.Rmd', package = 'anim.plots')
-#  doc_dir <- 'path/to/somewhere/appropriate' # e.g. your Desktop folder
+#  rmd_file <- system.file("doc", "full", "anim.plots.Rmd", package = "anim.plots")
+#  doc_dir <- "path/to/somewhere/appropriate" # e.g. your Desktop folder
 #  rmarkdown::render(rmd_file, clean = FALSE, output_dir = doc_dir)
 
